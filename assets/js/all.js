a996925c59894cfaa620b04bdaf496b5585f4c64
lily <a0976198586@gmail.com>
2022-02-27 02:10:57 +0800

    Update 2022-02-26T18:10:55.805Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,33 @@
 "use strict";
+
+var prodocutsTitle = document.querySelector('.js_prodocuts_title'); // 商品標題
+
+var productNavbarItems = document.querySelectorAll('.js_product_navbar_item'); // 商品分類表
+// 商品渲染事件
+
+function clickProductRenderEvent() {
+  productNavbarItems.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      cancelChecked();
+
+      if (e.target.innerText === 'OPTICAL') {
+        item.setAttribute('style', 'border-bottom-color: #AA0601;');
+      } else if (e.target.innerText === 'SUNGLASSES') {
+        item.setAttribute('style', 'border-bottom-color: #AA0601;');
+      } else if (e.target.innerText === 'FUNCTIONAL') {
+        item.setAttribute('style', 'border-bottom-color: #AA0601;');
+      }
+    });
+  });
+} // 取消所有選取效果
+
+
+function cancelChecked() {
+  productNavbarItems.forEach(function (item) {
+    item.removeAttribute('style');
+  });
+} //呼叫上面的這個函式
+
+
+clickProductRenderEvent();
 //# sourceMappingURL=all.js.map
