2ce62042aeafdbcbb8a26a58cc1cdd2bd5470d08
lily <a0976198586@gmail.com>
2022-02-27 16:22:34 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,8 +1,10 @@
 $(function(){
   $('.js_product_navbar_item').on('click', function(){
     const checkedClass = 'border-bottom-secondary';
+    const delCheckedClass = 'nav_item_content--checked';
 
     $('.js_product_navbar_item').addClass(checkedClass);
+    $(this).removeClass(delCheckedClass);
   });
 });
 
