b339970815223a1498e4bc75938fb9422b8a439d
lily <a0976198586@gmail.com>
2022-02-27 16:02:43 +0800

    update

---
+++ app/assets/js/all.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.js_product_navbar_item').on('click', function(){
+    const checkedClass = 'border-bottom-secondary';
+
+    $('.js_product_navbar_item').addClass(checkedClass);
+  });
+});
+
+
