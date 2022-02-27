1cda76f4838678b6e235289f27fa4a390767275c
lily <a0976198586@gmail.com>
2022-02-27 20:36:02 +0800

    Update 2022-02-27T12:36:00.398Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,12 +1,2 @@
 "use strict";
-
-// nav選項的 hover 格子裡下底紅線設定 (相關連結 _nav.scss 及 frame-nav.ejs)
-$(function () {
-  $('.js_product_navbar_item').on('click', function () {
-    var checkedClass = 'border-bottom-secondary';
-    var delCheckedClass = 'nav_item_content--checked';
-    $('.js_product_navbar_item').addClass(checkedClass);
-    $('.js_product_navbar_item').removeClass(delCheckedClass);
-  });
-});
 //# sourceMappingURL=all.js.map
