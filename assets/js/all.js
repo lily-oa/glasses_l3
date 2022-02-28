ec40fbe6a551e3b863e03fbb82b718d91e5de9f0
lily <a0976198586@gmail.com>
2022-02-28 15:35:46 +0800

    Update 2022-02-28T07:35:44.789Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,14 @@
 "use strict";
 
 $(function () {
+  $('.js_opt_click').on('click', function () {
+    location.href = './frame-opt.html';
+  });
   $('.js_sun_click').on('click', function () {
     location.href = './frame-sun.html';
+  });
+  $('.js_fun_click').on('click', function () {
+    location.href = './frame-fun.html';
   });
 });
 //# sourceMappingURL=all.js.map
